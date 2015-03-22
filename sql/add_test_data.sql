3151f135d83862254d7be82d77cadb714ce90743
Suvi <suvi.sipila@cs.helsinki.fi>
2015-03-22 17:36:23 +0200

    viikko 2

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,46 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+-- Hakutarkoitus-taulun testidata
+INSERT INTO Hakutarkoitus (nimi) VALUES
+('Kavereita'),
+('Seurustelua'),
+('Harrastusseuraa'),
+('Jotain ihan muuta');
+
+-- Kayttaja-taulun testidata
+INSERT INTO Kayttaja (kayttajatunnus, nimi, salasana, syntymaaika, sukupuoli, paikkakunta, 
+omatTiedot, hakutarkoitusID, oikeudet) VALUES
+('Misu', 'Miisa', 'Miisa3', '1990-04-15', 'N', 'Helsinki', 'Iloinen ja eläinrakas ...', 1, false),
+('Salamanteri', 'Saku', 'Saku1', '1991-03-02', 'M', 'Turku', 'Persoonallinen', 3, false);
+
+-- JulkinenProfiilisivu-taulun testidata
+INSERT INTO JulkinenProfiilisivu (kayttajaID, sisalto) VALUES
+(5, 'Minun profiilisivut: ja mitä kaikkea tänne sitten voi tulostella..'),
+(6, 'Minun profiilisivut: ja mitä kaikkea tänne sitten voi tulostella..');
+
+-- SalainenSivu-taulun testidata
+INSERT INTO SalainenSivu (kayttajaID, sisalto) VALUES
+(5, 'Salaisuuteni: En ole alunperin Helsingistä');
+
+-- Kiinnostukset-taulun testidata
+INSERT INTO Kiinnostukset (nimi) VALUES
+('Eläimet'),
+('Jääkiekko'),
+('Matkustelu');
+
+-- Kohteet-taulun testidata
+INSERT INTO Kohteet (kayttajaID, kiinnostusID) VALUES
+(5, 1),
+(5, 3),
+(6, 2);
+
+-- Viestit-taulun testidata
+INSERT INTO Viesti (aihe, sisalto, aika, luettu) VALUES
+('Terve', 'Lähtisitkö 15.5 klo 12 jonnekkin ...', NOW(), false),
+('Profiilisi herätti minussa toiveen', '.. siitä että ihmisiä on olemassa.', '2015-03-15', true),
+('Lenkille?', 'Mennäänkö lenkille?', '2015-03-22', false); 
+
+-- Vastaanottaja-taulun testidata
+INSERT INTO Vastaanottaja (viestiID, kayttajaID) VALUES
+(1, 6),
+(2, 5),
+(3, 5);
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,54 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Hakutarkoitus(
+id SERIAL PRIMARY KEY, -- pääavain kokonaisluku
+nimi varchar(100) NOT NULL -- millaista seuraa haetaan
+);
+
+CREATE TABLE Kayttaja(
+id SERIAL PRIMARY KEY, -- pääavain kokonaisluku
+kayttajatunnus varchar(20) NOT NULL, -- käyttäjätunnus
+nimi varchar(30) NOT NULL, -- käyttäjän etunimi
+salasana varchar(20) NOT NULL, -- käyttäjätunnuksen salasana
+syntymaaika DATE, -- käyttäjän syntymäaika
+sukupuoli varchar(1) NOT NULL, -- N = Nainen, M = Mies
+paikkakunta varchar(100) NOT NULL, -- paikkakunnan nimi
+omatTiedot varchar(300), -- käyttäjän vapaamuotoinen kuvaus itsestään
+kuva BYTEA, -- profiilikuva
+hakutarkoitusID INTEGER REFERENCES Hakutarkoitus(id), -- viiteavain Hakutarkoitus-tauluun
+oikeudet boolean -- true = pääkäyttäjä, false = peruskäyttäjä
+);
+
+CREATE TABLE JulkinenProfiilisivu(
+id SERIAL PRIMARY KEY, -- pääavain kokonaisluku
+kayttajaID INTEGER REFERENCES Kayttaja(id), -- viiteavain Kayttaja-tauluun
+sisalto varchar(900) NOT NULL -- sivun sisältö
+);
+
+CREATE TABLE SalainenSivu(
+id SERIAL PRIMARY KEY, -- pääavain kokonaisluku
+kayttajaID INTEGER REFERENCES Kayttaja(id), -- viiteavain Kayttaja-tauluun
+sisalto varchar(500) -- sivun sisältö
+);
+
+CREATE TABLE Kiinnostukset(
+id SERIAL PRIMARY KEY, -- pääavain kokonaisluku
+nimi varchar(100) -- kiinnostuksen nimi, esim. eläimet
+);
+
+CREATE TABLE Kohteet(
+kayttajaID INTEGER REFERENCES Kayttaja(id), -- viiteavain Kayttaja-tauluun
+kiinnostusID INTEGER REFERENCES Kiinnostukset(id) -- viiteavain Kiinnostukset-tauluun
+);
+
+CREATE TABLE Viesti(
+id SERIAL PRIMARY KEY, -- pääavain kokonaisluku
+aihe varchar(150), -- viestin aihe
+sisalto varchar(500), -- viestin sisältö
+aika DATE, -- milloin viesti on lähetetty
+luettu boolean -- true = luettu, false = lukematon
+);
+
+CREATE TABLE Vastaanottaja(
+viestiID INTEGER REFERENCES Viesti(id), -- viiteavain Viesti-tauluun
+kayttajaID INTEGER REFERENCES Kayttaja(id) -- viiteavain Kayttaja-tauluun
+);
